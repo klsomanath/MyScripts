5e16c15527497d0c5bc9184adbea54e8fc3f2eb2
Kornepati Lakshmi Somanath <90393227+klsomanath@users.noreply.github.com>
2024-07-21 19:11:04 +0530

    Update last-successful-commit.sh

--- Shell/last-successful-commit.sh
+++ Shell/last-successful-commit.sh
@@ -1,11 +1,11 @@
-## Get the Work-flow file for ehich you want to get the statuses
+## Get the Work-flow file for which you want to get the statuses
 workflow_file="example.yml"
 
-token="<Paste your SF Org Token>"
+token="<Paste your GitHub Org Token>"
 
 owner="<Paste your github username>"
 
-repo="<Paste your github repository name>"
+repo="<Paste your GitHub repository name>"
 
 last_successful_commit=$((curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $token " -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$owner/$repo/actions/workflows/$workflow_file/runs) | jq -r '.workflow_runs[] | select(.status == "completed" ) | select(.conclusion == "success") | .head_sha' | head -1 )
 
